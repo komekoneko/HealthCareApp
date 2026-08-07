c4816e42596b72ebdfb7bedbaf08c76aed1a9fce
komekoneko <kai.s20050701@gmail.com>
2026-08-07 23:30:08 +0900

    add 123~127 section クラスFood

--- dist/food-app.js
+++ dist/food-app.js
@@ -1,51 +1,46 @@
-class Score { 
+"use strict";
+class Score {
     get totalScore() {
         const foods = new Foods();
-        return foods.activeElementsScore.reduce((total, score) => total + score, 0)
-    }
-    render() {
-        document.querySelector('.score__number')!.textContent = String(this.totalScore);
+        return foods.activeElementsScore.reduce((total, score) => total + score, 0);
     }
 }
 class Food {
-    constructor(public element: HTMLDivElement) { 
+    constructor(element) {
+        this.element = element;
         element.addEventListener('click', this.clickEventHandler.bind(this));
     }
     clickEventHandler() {
-        this.element.classList.toggle('food--active')
+        this.element.classList.toggle('food--active');
     }
-    
- }
-class Foods { 
-    elements = document.querySelectorAll<HTMLDivElement>('.food');
-    private _activeElements: HTMLDivElement[] = [];
-    private _activeElementsScore: number[] = [];
+}
+class Foods {
     get activeElements() {
         this._activeElements = [];
         this.elements.forEach(element => {
-            if(element.classList.contains('food--active')) {
+            if (element.classList.contains('food--active')) {
                 this._activeElements.push(element);
             }
-        })
+        });
         return this._activeElements;
     }
     get activeElementsScore() {
         this._activeElementsScore = [];
         this.activeElements.forEach(element => {
             const foodScore = element.querySelector('.food__score');
-            if(foodScore) {
-                this._activeElementsScore.push(Number(foodScore.textContent))
+            if (foodScore) {
+                this._activeElementsScore.push(Number(foodScore.textContent));
             }
-        })
+        });
         return this._activeElementsScore;
-
     }
     constructor() {
+        this.elements = document.querySelectorAll('.food');
+        this._activeElements = [];
+        this._activeElementsScore = [];
         this.elements.forEach(element => {
             new Food(element);
-        })
+        });
     }
 }
 const foods = new Foods();
-
-
