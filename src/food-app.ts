fbc63d60e7ad164d70590ec4ac5b509418263750
komekoneko <kai.s20050701@gmail.com>
2026-08-07 19:09:00 +0900

    add 118~122 クラスFoods

--- src/food-app.ts
+++ src/food-app.ts
@@ -1,3 +1,16 @@
 class Score { }
-class Food { }
-class Foods { }
+class Food {
+    constructor(public element: HTMLDivElement) { 
+        element.addEventListener('click');
+    }
+    
+ }
+class Foods { 
+    elements = document.querySelectorAll<HTMLDivElement>('.food');
+    constructor() {
+        this.elements.forEach(element => {
+            new Food(element);
+        })
+    }
+}
+const foods = new Foods();
