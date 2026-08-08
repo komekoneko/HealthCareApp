7e245d47c2fcf56e97a1ef150e93d5c5f90ef6f6
komekoneko <kai.s20050701@gmail.com>
2026-08-08 15:45:33 +0900

    add 131~135 section module途中

---
+++ dist/main.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const foods_1 = require("./foods");
+foods_1.Foods.getInstance();
---
+++ src/food-app/food.ts
@@ -0,0 +1,15 @@
+import { Foodable } from "./interfaces.js"
+import { Score } from "./score.js"
+
+
+export class Food implements Foodable {
+  constructor(public element: HTMLDivElement) {
+    element.addEventListener("click", this.clickEventHandler.bind(this));
+  }
+  clickEventHandler() {
+    console.log("clicked");
+    this.element.classList.toggle("food--active");
+    const score = Score.getInstance();
+    score.render();
+  }
+}
